ba82edf32b2b4a24705d325e56970a7a34f8dcba
khaleela3mer <khaleel.a3mer@gmail.com>
2025-06-18 16:03:21 +0300

    Create QuizForm.tsx

---
+++ components/builder/QuizForm.tsx
@@ -0,0 +1,7 @@
+export default function QuizForm() {
+  return (
+    <div className="p-4 border rounded-lg bg-white">
+      <p className="text-gray-500">Quiz form will go here.</p>
+    </div>
+  );
+}
