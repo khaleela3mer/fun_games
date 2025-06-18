b80af57f32a5e34df70d5edb254b422303ddce27
khaleela3mer <khaleel.a3mer@gmail.com>
2025-06-18 16:04:16 +0300

    Create ClozeForm.tsx

---
+++ components/builder/ClozeForm.tsx
@@ -0,0 +1,7 @@
+export default function ClozeForm() {
+  return (
+    <div className="p-4 border rounded-lg bg-white">
+      <p className="text-gray-500">Cloze form will go here.</p>
+    </div>
+  );
+}
