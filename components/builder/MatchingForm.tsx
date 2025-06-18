143168572a8c6b63e6c6f21c8ec99cd16b6e3167
khaleela3mer <khaleel.a3mer@gmail.com>
2025-06-18 16:03:55 +0300

    Create MatchingForm.tsx

---
+++ components/builder/MatchingForm.tsx
@@ -0,0 +1,7 @@
+export default function MatchingForm() {
+  return (
+    <div className="p-4 border rounded-lg bg-white">
+      <p className="text-gray-500">Matching form will go here.</p>
+    </div>
+  );
+}
